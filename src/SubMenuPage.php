148082609c829e442254f500fe378ae8affcbede
André Keher <andrekeher@aprendiz.org.br>
2019-10-10 15:53:00 -0300

    Initial commit

---
+++ src/SubMenuPage.php
@@ -0,0 +1,22 @@
+<?php
+namespace AndreKeher\WPDP;
+
+use AndreKeher\WPDP\MenuPage;
+
+class SubMenuPage extends MenuPage
+{
+    protected $parentSlug;
+
+    public function __construct(
+        $parentSlug,
+        $pageTitle,
+        $menuTitle,
+        $menuSlug,
+        $capability = 'manage_options',
+        $iconUrl = '',
+        $position = null
+    ) {
+        $this->parentSlug = $parentSlug;
+        parent::__construct($pageTitle, $menuTitle, $menuSlug, $capability, $iconUrl, $position);
+    }
+}
